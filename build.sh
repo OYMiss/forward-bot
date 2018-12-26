c3f9944c74fd90b20ed6088f980969d58380af33
OYMiss <oy.miss@outlook.com>
2018-12-26 15:37:37 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 git clone https://github.com/OYMiss/forward-bot.git
 cd forward-bot
-docker build -t forward-bot:alpha .
-docker tag forward-bot:alpha oymiss/forward-bot:alpha
+docker build -t oymiss/forward-bot:alpha .
 docker push oymiss/forward-bot:alpha
