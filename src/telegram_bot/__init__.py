d4b17c4d9840d0464ab3d0626cfc73030f966456
OYMiss <oy.miss@outlook.com>
2019-10-21 12:50:25 +0800

    fix contact list bug

--- src/telegram_bot/__init__.py
+++ src/telegram_bot/__init__.py
@@ -1,3 +1,4 @@
+from math import ceil
 from collections import defaultdict
 
 from constant import TELEGRAM_ID, ENABLE_PROXY
@@ -44,13 +45,22 @@ class TelegramBot:
         self.current_receiver: Contact = None
         self.contact_mapped_by_message_id = defaultdict(Contact)
         self.messages_mapped_by_contact_id = defaultdict(list)
+        self.contact_page_message = None
         self.contact_map = contact_map
-
+        self.contact_curpage = 0
+        self.contact_pages = self.get_contact_pages()
         self.cloud = cloud
         self.target_key = target_key
         self.updater = config.create_updater()
         self.init_updater()
         self.cloud.set_target(target_key, self)
+
+    def get_contact_pages(self):
+        # 每 50 人一页
+        contact_pages = [list() for _ in range(ceil(len(self.contact_map.values()) / 30))]
+        for i, contact in enumerate(self.contact_map.values()):
+            contact_pages[i // 30].append(contact)
+        return contact_pages
 
     def init_updater(self):
         self.updater.dispatcher.add_handler(MessageHandler(Filters.text, self.on_receive))
@@ -59,7 +69,6 @@ class TelegramBot:
         self.updater.dispatcher.add_handler(CommandHandler("current", self.show_current))
 
         self.updater.start_polling()
-        self.show_contacts(None, None)
         self.updater.bot.send_message(chat_id=TELEGRAM_ID, text="Ready")
 
     def send_info(self, text: str):
@@ -76,15 +85,26 @@ class TelegramBot:
     def make_contact_list_keyboard(self):
         keyboard = []
         cur_row = []
-        for contact in self.contact_map.values():
-            cur_row.append(make_inline_keyboard(contact.name, contact.id))
-            if len(cur_row) == 3:
-                keyboard.append(cur_row.copy())
-                cur_row.clear()
+        if self.contact_pages and self.contact_curpage < len(self.contact_pages):
+            for contact in self.contact_pages[self.contact_curpage]:
+                cur_row.append(make_inline_keyboard(contact.name, contact.id))
+                if len(cur_row) == 3:
+                    keyboard.append(cur_row.copy())
+                    cur_row.clear()
 
-        if len(cur_row) > 0:
-            keyboard.append(cur_row)
-        return keyboard
+            if len(cur_row) > 0:
+                keyboard.append(cur_row.copy())
+            if len(self.contact_pages) > 1:
+                cur_row.clear()
+                cur_row.append(make_inline_keyboard(str(self.contact_curpage + 1) + "/" + str(len(self.contact_pages)), "cmd_none"))
+                cur_row.append(make_inline_keyboard("<<", "cmd_first_page"))
+                cur_row.append(make_inline_keyboard("<", "cmd_pre_page"))
+                cur_row.append(make_inline_keyboard(">", "cmd_next_page"))
+                cur_row.append(make_inline_keyboard(">>", "cmd_last_page"))
+                keyboard.append(cur_row.copy())
+            return keyboard
+        else:
+            return keyboard
 
     def show_current(self, bot, update):
         if self.current_receiver is None:
@@ -93,12 +113,28 @@ class TelegramBot:
         self.send_info("当前会话是「{0}」。".format(self.current_receiver.name))
 
     def show_contacts(self, bot, update):
+        self.contact_curpage = 0
         self.cloud.refresh_coolq()
-        self.send_inline_keyboard("所有联系方式", self.make_contact_list_keyboard())
+        self.contact_page_message = self.send_inline_keyboard("所有联系方式", self.make_contact_list_keyboard())
 
     def on_button_click(self, bot, update):
-        callback_data = int(update.callback_query.data)
-        self.change_receiver(self.contact_map[callback_data])
+        callback_data = update.callback_query.data
+        if callback_data.startswith("cmd"):
+            len(self.contact_pages) - 1
+            if callback_data == "cmd_next_page":
+                self.contact_curpage = min(self.contact_curpage + 1, len(self.contact_pages) - 1)
+            elif callback_data == "cmd_pre_page":
+                self.contact_curpage = max(self.contact_curpage - 1, 0)
+            elif callback_data == "cmd_first_page":
+                self.contact_curpage = 0
+            elif callback_data == "cmd_last_page":
+                self.contact_curpage = len(self.contact_pages) - 1
+
+            if self.contact_page_message:
+                self.contact_page_message.edit_reply_markup(reply_markup=InlineKeyboardMarkup(self.make_contact_list_keyboard()))
+        else:
+            callback_data = int(update.callback_query.data)
+            self.change_receiver(self.contact_map[callback_data])
 
     def on_receive(self, bot, update):
         if update.message.chat_id != TELEGRAM_ID:
